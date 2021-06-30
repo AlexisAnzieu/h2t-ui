1c98b2b9d68a02bcb3a2b2f31dad899db31acc3d
alexis anzieu <alexis.anzieu@ssense.com>
2021-06-30 17:16:24 -0400

    check refresh

--- api/openDoor.js
+++ api/openDoor.js
@@ -1,7 +1,7 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 module.exports = async (_, res) => {
-    const result = await fetch(`http://${STATIC_IP}:8080/api`);
-    const data = await result.json();
-    res.status(200).send(data);
+    // const result = await fetch(`http://${STATIC_IP}:8080/api`);
+    // const data = await result.json();
+    res.status(200).send("coucou");
 };
