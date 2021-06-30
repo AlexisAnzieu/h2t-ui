773830da246c7d3d404ae6facf68719b2fb79ce4
alexis anzieu <alexis.anzieu@ssense.com>
2021-06-30 17:19:34 -0400

    fix process env

--- api/openDoor.js
+++ api/openDoor.js
@@ -1,7 +1,7 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-module.exports = async (_, res) => {
-    // const result = await fetch(`http://${STATIC_IP}:8080/api`);
-    // const data = await result.json();
-    res.status(200).send("coucou");
+export default async (_, res) => {
+    const result = await fetch(`http://${process.env.STATIC_IP}:8080/api`);
+    const data = await result.json();
+    res.status(200).send(data);
 };
