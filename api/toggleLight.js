87788c853495c21b6a43446242c31720298f90df
alexis anzieu <alexis.anzieu@ssense.com>
2021-10-19 11:34:20 -0400

    good json

--- api/toggleLight.js
+++ api/toggleLight.js
@@ -4,5 +4,5 @@ export default async (_, res) => {
     const result = await fetch(`https://maker.ifttt.com/trigger/h2t_door_open/with/key/${process.env.IFTT_KEY}`);
     const data = await result;
     console.log(data)
-    res.status(200).send(data);
+    res.status(200).send({ result: data });
 };
