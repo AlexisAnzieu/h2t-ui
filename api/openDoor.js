d167a06cb066ff26981ca26e6e030136dc334c6b
alexis anzieu <alexis.anzieu@ssense.com>
2021-10-19 11:12:42 -0400

    add log

--- api/openDoor.js
+++ api/openDoor.js
@@ -3,5 +3,6 @@ import fetch from "node-fetch";
 export default async (_, res) => {
     const result = await fetch(`http://${process.env.STATIC_IP}:8080/api`);
     const data = await result.json();
+    console.log(data)
     res.status(200).send(data);
 };
--- api/toggleLight.js
+++ api/toggleLight.js
@@ -3,5 +3,6 @@ import fetch from "node-fetch";
 export default async (_, res) => {
     const result = await fetch(`https://maker.ifttt.com/trigger/h2t_door_open/with/key/${process.env.IFTT_KEY}`);
     const data = await result.json();
+    console.log(data)
     res.status(200).send(data);
 };
