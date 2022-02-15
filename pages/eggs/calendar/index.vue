7743233ac959c6c3df23049511059bd4298148ef
alexis anzieu <alexis.anzieu@ssense.com>
2022-02-15 18:31:41 -0500

    bigger calendar

--- pages/eggs/calendar/index.vue
+++ pages/eggs/calendar/index.vue
@@ -5,7 +5,7 @@
                 src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=America%2FToronto&mode=MONTH&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=0&showTz=0&src=YzNhZ2hyNWxua2JubWtsM2ltaXEyM2pmdnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%238E24AA"
                 style="border-width: 0"
                 width="80%"
-                height="400px"
+                height="500px"
                 frameborder="0"
                 scrolling="no"
             ></iframe>
