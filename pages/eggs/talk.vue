a4ee28a20f54c0d98bf504576e3abefd98cfbfc8
alexis anzieu <alexis.anzieu@ssense.com>
2022-03-30 16:46:19 -0400

    push new talk

--- pages/eggs/talk.vue
+++ pages/eggs/talk.vue
@@ -2,6 +2,127 @@
     <div class="body">
         <el-col :lg="{ span: 21, offset: 1 }" :md="24" style="padding: 10px">
             <el-collapse v-model="activeName" accordion>
+                <el-collapse-item title="H2TALK UN" name="un">
+                    <template slot="title">
+                        <h2 style="text-align: center">H2TALK UN</h2>
+                    </template>
+                    <el-card class="box-card">
+                        <div class="description">
+                            <div style="text-align: left; margin-bottom: 20px">
+                                SAISON Deux: Zéro préparation, zéro sujet
+                                prédéfini, zéro expérience. Tour de passe-passe
+                                écologique, spirituel, éducatif et rêveur entre
+                                les copains Thomas, Alexis, Victor, Pierre et
+                                Aymeric
+                            </div>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/49adm7U6GQvgdmZVqafQXJ"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/5DH1sMtvMcK0KCURBsX697"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/0ELwJXor5aUuW2dQ8Oohz5"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/61zVQ6G0ncaKrxJzn27Lhn"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/6UXYtP7UsG4ycOsNA3S2Id"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/0gKds7yfOSzo3DnTtFmCtB"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/5mdYhh0WDlhIBldHpTvyca"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/0mj5Lf5WRQOU4uuuWA3RX5"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/39ZUAsuhSHV9vR4DCr5pSA"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/7bfDr3iTxhvYSzmaOgsNrH"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S01"
+                                src="https://open.spotify.com/embed-podcast/episode/1sEnyJyNdqCjcvd00qBme5"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/5SEqcCRjlaA9uNWRMG8NTt"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/036oBwWq7fSJydb3J4PogX"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/4nmtvbbdkpnNsWGKJn3roy"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                            <iframe
+                                title="S02"
+                                src="https://open.spotify.com/embed-podcast/episode/6npLwekKhr7NYzr23Qu6kn"
+                                :width="$device.isMobile ? '100%' : '60%'"
+                                height="232"
+                                allow="encrypted-media"
+                            ></iframe>
+                        </div>
+                    </el-card>
+                </el-collapse-item>
                 <el-collapse-item title="H2TALK ZERO" name="zero">
                     <template slot="title">
                         <h2 style="text-align: center">H2TALK ZERO</h2>
@@ -109,7 +230,7 @@ export default {
     auth: false,
     data() {
         return {
-            activeName: "zero",
+            activeName: "un",
         };
     },
 };
